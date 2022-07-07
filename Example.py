1f6c6688abf70966ceb2ba9184a8f0e25fd8fbe8
nirmalk-git <nirmalk90@gmail.com>
2022-07-07 14:59:13 +0200

    Second commit, new code added

---
+++ Example.py
@@ -0,0 +1,26 @@
+def Multiplication(arg1, arg2):
+    """
+    Multiplication of arg1 and arg2, return multiplied value
+    :param arg1:
+    :type arg1:
+    :param arg2:
+    :type arg2:
+    :return:
+    :rtype:
+    """
+    mul = arg1*arg2
+    return mul
+
+
+def division(arg1, arg2):
+    """
+    Return the division of arg1 and arg2
+    :param arg1:
+    :type arg1:
+    :param arg2:
+    :type arg2:
+    :return:
+    :rtype:
+    """
+    div = arg1/arg2
+    return div 
